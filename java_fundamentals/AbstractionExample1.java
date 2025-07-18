4a63b7ae4a2e63cf696d6a6849ead6ac74638ff7
shiva1882 <shivakuncham29@gmail.com>
2025-07-18 10:05:53 +0530

    Update AbstractionExample1.java

--- java_fundamentals/AbstractionExample1.java
+++ java_fundamentals/AbstractionExample1.java
@@ -1,1 +1,21 @@
-.
+abstract class Animal {
+    abstract void makeSound();
+
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractionExample1 {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.makeSound();
+        dog.sleep();
+    }
+}
