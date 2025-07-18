4df362ee87a104d739c2388b1b796d2870bc70a0
shiva1882 <shivakuncham29@gmail.com>
2025-07-18 12:07:39 +0530

    Create BinaryRepresentation .java

---
+++ java_fundamentals/BinaryRepresentation .java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.Scanner;
+
+public class BinaryRepresentation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer (1-255): ");
+        int number = scanner.nextInt();
+
+        if (number < 1 || number > 255) {
+            System.out.println("Please enter a number between 1 and 255.");
+        } else {
+            // Convert to binary string
+            String binaryString = Integer.toBinaryString(number);
+
+            // Pad with leading zeros to make 8 digits
+            binaryString = String.format("%8s", binaryString).replace(' ', '0');
+
+            System.out.println("Binary representation: " + binaryString);
+        }
+
+        scanner.close();
+    }
+}
