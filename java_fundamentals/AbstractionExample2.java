98f044f3ae4bede4e95dee7ccf24bce922284917
shiva1882 <shivakuncham29@gmail.com>
2025-07-18 11:10:31 +0530

    Create AbstractionExample2.java

---
+++ java_fundamentals/AbstractionExample2.java
@@ -0,0 +1,17 @@
+
+interface Vehicle {
+    void start();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+}
+
+public class AbstractionExample2 {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        car.start();
+    }
+}
